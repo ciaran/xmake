66590999c3821322eed78d3534192df7df8b6fff
Ciarán Walsh <git@ciaranwal.sh>
2009-03-27 17:00:15 +0000

    Link file:line messages in exception backtraces

--- src/OutputParser.mm
+++ src/OutputParser.mm
@@ -23,6 +23,18 @@
 	return [NSString stringWithFormat:@"txmt://open?url=file://%@&line=%d", path, lineNumber];
 }
 
+- (NSString*)pathForFilename:(NSString*)filename inDirectory:(NSString*)directory
+{
+	NSTask* task        = [[NSTask new] autorelease];
+	task.launchPath     = @"/usr/bin/find";
+	task.arguments      = [NSArray arrayWithObjects:directory, @"-name", filename, nil];
+	[task setStandardOutput:[NSPipe pipe]];
+	[task launch];
+	[task waitUntilExit];
+	NSString* result = [[[NSString alloc] initWithData:[[[task standardOutput] fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding] autorelease];
+	return [[result componentsSeparatedByString:@"\n"] objectAtIndex:0];
+}
+
 - (NSAttributedString*)processOutput:(NSString*)output
 {
 	static NSDictionary* const attributes = [[NSDictionary alloc] initWithObjectsAndKeys:[NSFont fontWithName:@"DejaVuSansMono" size:11], NSFontAttributeName, nil];
@@ -50,6 +62,31 @@
 			[pathString addAttribute:NSLinkAttributeName value:[self openLinkForFile:path line:lineNumber] range:NSMakeRange(0, pathString.length)];
 			[string replaceCharactersInRange:matchedRange withAttributedString:pathString];
 			matchedRange.location += pathString.length + 1;
+		}
+	}
+
+	if(self.sourcePath)
+	{
+		// Look for “filename:line” from exception backtraces
+		matchedRange = (NSRange){0, 0};
+		while(matchedRange.location < string.string.length)
+		{
+			NSRange searchRange = NSMakeRange(matchedRange.location, string.string.length - matchedRange.location);
+			matchedRange = [string.string rangeOfRegex:@"^\\s*([^/].+?):(\\d+)"
+	                                           options:RKLMultiline
+	                                           inRange:searchRange
+	                                           capture:0
+	                                             error:NULL];
+			if(matchedRange.location != NSNotFound)
+			{
+				NSArray* components   = [[string.string substringWithRange:matchedRange] componentsSeparatedByString:@":"];
+				NSString* filename    = [[components objectAtIndex:0] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+				NSUInteger lineNumber = [[components objectAtIndex:1] intValue];
+
+				if(NSString* path = [self pathForFilename:filename inDirectory:self.sourcePath])
+					[string addAttribute:NSLinkAttributeName value:[self openLinkForFile:path line:lineNumber] range:matchedRange];
+				matchedRange.location += matchedRange.length + 1;
+			}
 		}
 	}
 
